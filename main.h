779aeca4a1eacb9e227941ac0234f5663b80b252
Victor Osibajo <victoroshi@gmail.com>
2023-07-01 03:44:32 +0000

    fixing betty style error in main.h

--- main.h
+++ main.h
@@ -8,39 +8,39 @@
 #include <unistd.h>
 
 /**
- * struct format - conversion specifiers for printf
- * @id: specifier char pointer
- * @f: type pointer to function of the conversion specifier
- *
- */
+* struct format - User define type for  the print format
+* @id: id to be used
+* @f: pointer
+*/
 typedef struct format
 {
 	char *id;
 	int (*f)();
+
 } convert_match;
 
-int _putchar(char c);
-int printf_37(void);
-int printf_string(va_list val);
-int _strlenc(const char *s);
-int *_strcpy(char *dest, char *src);
-int printf_HEX(va_list val);
-int printf_hex(va_list val);
-int _strlen(char *s);
-int printf_dec(va_list args);
-int printf_int(va_list args);
-int printf_exclusive_string(va_list val);
-int printf_HEX_aux(unsigned int num);
-int printf_rot13(va_list args);
-int printf_hex_aux(unsigned long int num);
-int printf_pointer(va_list val);
-int printf_srev(va_list args);
-int printf_bin(va_list val);
-int printf_unsigned(va_list args);
-int printf_oct(va_list val);
 int _printf(const char *format, ...);
-int printf_char(va_list val);
-int rev_string(char *s);
+int _putchar(char c);
+int print_dec(va_list args);
+int print_int(va_list args);
+int print_string(va_list arg);
+int print_char(va_list arg);
+int print_37(void);
+int print_HEX_aux(unsigned int numb);
+int print_HEX(va_list arg);
 int _strlenc(const char *s);
+int _strlen(char *s);
+int print_bi(va_list arg);
+int print_hex_aux(unsigned long int numb);
+int print_hex(va_list arg);
+int print_unsigned(va_list args);
+int print_pointer(va_list arg);
+int print_rot13(va_list args);
+int print_hex_aux(unsigned long int numb);
+int print_exclusive_string(va_list arg);
+int print_oct(va_list arg);
+int print_srev(va_list args);
+int *_strcpy(char *dest, char *src);
+int rev_string(char *s);
 
 #endif
